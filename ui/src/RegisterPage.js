4308b82e75c20d66291089d06421ad9e3015d91b
hinst <hinstance@yandex.ru>
2017-05-22 17:49:33 +0300

    added RegisterPage

---
+++ ui/src/RegisterPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class RegisterPage extends React.Component {
+
+	constructor() {
+		super();
+	}
+
+	render() {
+		return(
+			<div className="w3-container">
+			</div>
+		);
+	}
+
+}
+
+export default RegisterPage;
